9284e58198a58b4f119cffd88bfc952314ffce6c
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-07 17:38:13 -0400

    Start on linking objects

--- make.py
+++ make.py
@@ -8,6 +8,8 @@ class CompileC:
         self.src_dir = 'src'
         self.obj_dir = 'obj'
 
+        self.exec = 'nand'
+
         self.debug_flags = ['-g']
         self.std_flags = ['-std=c17']
         self.include_flags = ['-Iinclude', '-Iunity']
@@ -15,16 +17,31 @@ class CompileC:
 
         self.flags = self.debug_flags + self.std_flags + self.include_flags + self.warnings_flags
 
-    def compile_source_to_object(self, src_path, obj_path):
-        """Compile a single source file to an object file."""
-        command = ['gcc'] + self.flags +  ['-c', src_path, '-o', obj_path]
-        command = ['gcc', '---v']
+    def _run_command(self, command):
         try:
             subprocess.run(command, check=True, capture_output=False, text=True)
         except subprocess.CalledProcessError:
-            pass
+            print(f"Failed to run {command}")
+
+    def compile_source(self, src_path, obj_path):
+        """Compile a single source file to an object file."""
+        command = ['gcc'] + self.flags +  ['-c', src_path, '-o', obj_path]
+        self._run_command(command)
+
+    def compile_sources(self):
+        self.get_source_files()
+        for src_file, obj_file in zip(self.src_files, self.obj_files):
+            self.compile_source(src_file, obj_file)
+
+    def link_objects(self):
+        pass
+
+
+    def get_source_files(self):
+        self.src_files = glob.glob(self.src_dir + '/*.c')
+        self.obj_files = [file.replace('.c', '.o') for file in self.src_files]
 
 
 if __name__ == "__main__":
     cc = CompileC()
-    cc.compile_source_to_object("src/gates.c", "obj/gates.o")
+    cc.compile_sources()
