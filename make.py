754597a84bc8566df87278e8a3ee33ebdcfa7081
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-07 17:50:27 -0400

    Add a bit more to the python make file

--- make.py
+++ make.py
@@ -8,6 +8,8 @@ class CompileC:
         self.src_dir = 'src'
         self.obj_dir = 'obj'
         self.bin_dir = 'bin'
+        self.test_dir = 'test'
+        self.obj_test_dir = os.path.join('bin', 'tests')
 
         self.output_executable = os.path.join(self.bin_dir, 'nand')
 
@@ -24,7 +26,7 @@ class CompileC:
         except subprocess.CalledProcessError:
             print(f"Failed to run {command}")
 
-    def compile_source(self, src_path, obj_path):
+    def compile_file(self, src_path, obj_path):
         """Compile a single source file to an object file."""
         command = ['gcc'] + self.flags +  ['-c', src_path, '-o', obj_path]
         self._run_command(command)
@@ -37,13 +39,18 @@ class CompileC:
         obj_files = [file.replace('.c', '.o') for file in src_files]
 
         for src_file, obj_file in zip(src_files, obj_files):
-            self.compile_source(src_file, obj_file)
+            self.compile_file(src_file, obj_file)
+
+    def compile_tests(self):
+        if not os.path.exists(self.obj_test_dir):
+            os.makedirs(self.obj_test_dir)
 
     def link_objects(self):
         if not os.path.exists(self.bin_dir):
             os.makedirs(self.bin_dir)
-        obj_files_made = glob.glob(self.obj_dir + '/*.o')
-        command = ['gcc', '-o', self.output_executable] + obj_files_made
+
+        obj_files = glob.glob(self.obj_dir + '/*.o')
+        command = ['gcc', '-o', self.output_executable] + obj_files
         self._run_command(command)
 
     def run_program(self):
@@ -56,3 +63,5 @@ if __name__ == "__main__":
     cc.compile_sources()
     cc.link_objects()
     cc.run_program()
+    cc.compile_tests()
+    
--- src/main.c
+++ src/main.c
@@ -5,8 +5,6 @@
 int main()
 {
 	printf("%d\n", nand_gate(1, 0));
-	printf("%d\n", nand_gate(1, 0));
-	printf("%d\n", nand_gate(1, 0));
 
 	return 0;
 }
