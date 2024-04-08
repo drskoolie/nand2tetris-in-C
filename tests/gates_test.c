3f546eacb8a5d05cdc768328b656d7e7102e472b
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-08 09:26:16 -0400

    Add a blank test

--- tests/gates_test.c
+++ tests/gates_test.c
@@ -6,11 +6,17 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
+void test_nand_n()
+{
+	TEST_ASSERT_EQUAL_INT(1, 1);
+}
+
+
 
 int main(void) {
 	UNITY_BEGIN();
 
-//	RUN_TEST(test_nand_gate);
+	RUN_TEST(test_nand_n);
 
 	return UNITY_END();
 }
