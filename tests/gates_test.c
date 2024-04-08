5f19f78c47b5d71a290b28fe589f936e379c0fe3
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-08 15:59:16 -0400

    Add a little about test

--- tests/gates_test.c
+++ tests/gates_test.c
@@ -8,7 +8,19 @@ void tearDown(void) {}
 
 void test_nand_n(void)
 {
+	int no_of_bits = 4;
+	int input0_bits[no_of_bits] = {0, 0, 1, 1};
+	int input1_bits[no_of_bits] = {0, 1, 0, 1};
+	int output_bits[no_of_bits] = {0};
 
+	int correct_bits[no_of_bits] = {1, 1, 1, 0};
+
+	BitArray input0 = BitArray{input0_bits, no_of_bits};
+	BitArray input1 = BitArray{input1_bits, no_of_bits};
+	BitArray output = BitArray{output_bits, no_of_bits};
+	BitArray correct = BitArray{correct_bits, no_of_bits};
+
+	TEST_ASSERT_EQUAL_INT(1, 1);
 }
 
 int main(void) {
