8abdcbe15ccb10cde277e4c08beda665935fedad
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-08 16:55:01 -0400

    Add and

--- include/gates.h
+++ include/gates.h
@@ -3,6 +3,8 @@
 
 #include "bit_array.h"
 
+void raise_error(const char *message);
 void nand_n(BitArray input0, BitArray input1, BitArray *output);
+void and_n(BitArray input0, BitArray input1, BitArray *output);
 
 #endif
--- src/gates.c
+++ src/gates.c
@@ -10,11 +10,21 @@ void raise_error(const char *message) {
 void nand_n(BitArray input0, BitArray input1, BitArray *output)
 {
 	if (! ((input0.no_of_bits == input1.no_of_bits) && (input1.no_of_bits == output->no_of_bits)) ) {
-		printf("Hi");
 		raise_error("nand_n()'s no_of_bits is not equal between inputs\n");
 	}
 
 	for (int i = 0; i < input0.no_of_bits; i++) {
 		output->bits[i] = !(input0.bits[i] && input1.bits[i]);
+	}
+}
+
+void and_n(BitArray input0, BitArray input1, BitArray *output)
+{
+	if (! ((input0.no_of_bits == input1.no_of_bits) && (input1.no_of_bits == output->no_of_bits)) ) {
+		raise_error("nand_n()'s no_of_bits is not equal between inputs\n");
+	}
+
+	for (int i = 0; i < input0.no_of_bits; i++) {
+		output->bits[i] = (input0.bits[i] && input1.bits[i]);
 	}
 }
