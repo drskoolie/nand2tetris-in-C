e0a58d69da85688c8362939929e645aa9cb6f55f
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-08 17:03:48 -0400

    Add xor

--- include/gates.h
+++ include/gates.h
@@ -4,9 +4,10 @@
 #include "bit_array.h"
 
 void raise_error(const char *message);
-void nand_n(BitArray input0, BitArray input1, BitArray *output);
 void and_n(BitArray input0, BitArray input1, BitArray *output);
 void or_n(BitArray input0, BitArray input1, BitArray *output);
 void not_n(BitArray input, BitArray *output);
+void nand_n(BitArray input0, BitArray input1, BitArray *output);
+void xor_n(BitArray input0, BitArray input1, BitArray *output);
 
 #endif
--- tests/test_gates.c
+++ tests/test_gates.c
@@ -6,49 +6,6 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_nand_n_0(void)
-{
-	int no_of_bits = 4;
-	int input0_bits[] = {0, 0, 1, 1};
-	int input1_bits[] = {0, 1, 0, 1};
-	int output_bits[4] = {0};
-
-	int correct_bits[] = {1, 1, 1, 0};
-
-	BitArray input0 = {input0_bits, no_of_bits};
-	BitArray input1 = {input1_bits, no_of_bits};
-	BitArray output = {output_bits, no_of_bits};
-	BitArray correct = {correct_bits, no_of_bits};
-
-	nand_n(input0, input1, &output);
-
-	for (int i = 0; i < no_of_bits; i++) {
-		TEST_ASSERT_EQUAL_INT(correct.bits[i], output_bits[i]);
-	}
-}
-
-
-void test_nand_n_1(void)
-{
-	int no_of_bits = 6;
-	int input0_bits[] = {0, 0, 1, 1, 0, 0};
-	int input1_bits[] = {0, 1, 0, 1, 1, 0};
-	int output_bits[6] = {0};
-
-	int correct_bits[] = {1, 1, 1, 0, 1, 1};
-
-	BitArray input0 = {input0_bits, no_of_bits};
-	BitArray input1 = {input1_bits, no_of_bits};
-	BitArray output = {output_bits, no_of_bits};
-	BitArray correct = {correct_bits, no_of_bits};
-
-	nand_n(input0, input1, &output);
-
-	for (int i = 0; i < no_of_bits; i++) {
-		TEST_ASSERT_EQUAL_INT(correct.bits[i], output_bits[i]);
-	}
-}
-
 void test_and_n_0(void)
 {
 	int no_of_bits = 4;
@@ -175,6 +132,93 @@ void test_not_n_1(void)
 	}
 }
 
+
+void test_nand_n_0(void)
+{
+	int no_of_bits = 4;
+	int input0_bits[] = {0, 0, 1, 1};
+	int input1_bits[] = {0, 1, 0, 1};
+	int output_bits[4] = {0};
+
+	int correct_bits[] = {1, 1, 1, 0};
+
+	BitArray input0 = {input0_bits, no_of_bits};
+	BitArray input1 = {input1_bits, no_of_bits};
+	BitArray output = {output_bits, no_of_bits};
+	BitArray correct = {correct_bits, no_of_bits};
+
+	nand_n(input0, input1, &output);
+
+	for (int i = 0; i < no_of_bits; i++) {
+		TEST_ASSERT_EQUAL_INT(correct.bits[i], output_bits[i]);
+	}
+}
+
+
+void test_nand_n_1(void)
+{
+	int no_of_bits = 6;
+	int input0_bits[] = {0, 0, 1, 1, 0, 0};
+	int input1_bits[] = {0, 1, 0, 1, 1, 0};
+	int output_bits[6] = {0};
+
+	int correct_bits[] = {1, 1, 1, 0, 1, 1};
+
+	BitArray input0 = {input0_bits, no_of_bits};
+	BitArray input1 = {input1_bits, no_of_bits};
+	BitArray output = {output_bits, no_of_bits};
+	BitArray correct = {correct_bits, no_of_bits};
+
+	nand_n(input0, input1, &output);
+
+	for (int i = 0; i < no_of_bits; i++) {
+		TEST_ASSERT_EQUAL_INT(correct.bits[i], output_bits[i]);
+	}
+}
+
+
+void test_xor_n_0(void)
+{
+	int no_of_bits = 4;
+	int input0_bits[] = {0, 0, 1, 1};
+	int input1_bits[] = {0, 1, 0, 1};
+	int output_bits[4] = {0};
+
+	int correct_bits[] = {1, 1, 1, 0};
+
+	BitArray input0 = {input0_bits, no_of_bits};
+	BitArray input1 = {input1_bits, no_of_bits};
+	BitArray output = {output_bits, no_of_bits};
+	BitArray correct = {correct_bits, no_of_bits};
+
+	xor_n(input0, input1, &output);
+
+	for (int i = 0; i < no_of_bits; i++) {
+		TEST_ASSERT_EQUAL_INT(correct.bits[i], output_bits[i]);
+	}
+}
+
+
+void test_xor_n_1(void)
+{
+	int no_of_bits = 6;
+	int input0_bits[] = {0, 0, 1, 1, 0, 0};
+	int input1_bits[] = {0, 1, 0, 1, 1, 0};
+	int output_bits[6] = {0};
+
+	int correct_bits[] = {1, 1, 1, 0, 1, 1};
+
+	BitArray input0 = {input0_bits, no_of_bits};
+	BitArray input1 = {input1_bits, no_of_bits};
+	BitArray output = {output_bits, no_of_bits};
+	BitArray correct = {correct_bits, no_of_bits};
+
+	xor_n(input0, input1, &output);
+
+	for (int i = 0; i < no_of_bits; i++) {
+		TEST_ASSERT_EQUAL_INT(correct.bits[i], output_bits[i]);
+	}
+}
 
 int main(void) {
 	UNITY_BEGIN();
