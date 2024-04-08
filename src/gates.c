f0feb0b49098eb9e29828db971de91de35f138e5
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-08 16:57:45 -0400

    Add or gate

--- src/gates.c
+++ src/gates.c
@@ -21,10 +21,21 @@ void nand_n(BitArray input0, BitArray input1, BitArray *output)
 void and_n(BitArray input0, BitArray input1, BitArray *output)
 {
 	if (! ((input0.no_of_bits == input1.no_of_bits) && (input1.no_of_bits == output->no_of_bits)) ) {
-		raise_error("nand_n()'s no_of_bits is not equal between inputs\n");
+		raise_error("and_n()'s no_of_bits is not equal between inputs\n");
 	}
 
 	for (int i = 0; i < input0.no_of_bits; i++) {
 		output->bits[i] = (input0.bits[i] && input1.bits[i]);
+	}
+}
+
+void or_n(BitArray input0, BitArray input1, BitArray *output)
+{
+	if (! ((input0.no_of_bits == input1.no_of_bits) && (input1.no_of_bits == output->no_of_bits)) ) {
+		raise_error("or_n()'s no_of_bits is not equal between inputs\n");
+	}
+
+	for (int i = 0; i < input0.no_of_bits; i++) {
+		output->bits[i] = (input0.bits[i] || input1.bits[i]);
 	}
 }
--- tests/test_gates.c
+++ tests/test_gates.c
@@ -93,6 +93,50 @@ void test_and_n_1(void)
 }
 
 
+void test_or_n_0(void)
+{
+	int no_of_bits = 4;
+	int input0_bits[] = {0, 0, 1, 1};
+	int input1_bits[] = {0, 1, 0, 1};
+	int output_bits[4] = {0};
+
+	int correct_bits[] = {0, 1, 1, 1};
+
+	BitArray input0 = {input0_bits, no_of_bits};
+	BitArray input1 = {input1_bits, no_of_bits};
+	BitArray output = {output_bits, no_of_bits};
+	BitArray correct = {correct_bits, no_of_bits};
+
+	or_n(input0, input1, &output);
+
+	for (int i = 0; i < no_of_bits; i++) {
+		TEST_ASSERT_EQUAL_INT(correct.bits[i], output_bits[i]);
+	}
+}
+
+
+void test_or_n_1(void)
+{
+	int no_of_bits = 6;
+	int input0_bits[] = {0, 0, 1, 1, 0, 0};
+	int input1_bits[] = {0, 1, 0, 1, 1, 0};
+	int output_bits[6] = {0};
+
+	int correct_bits[] = {0, 1, 1, 1, 1, 0};
+
+	BitArray input0 = {input0_bits, no_of_bits};
+	BitArray input1 = {input1_bits, no_of_bits};
+	BitArray output = {output_bits, no_of_bits};
+	BitArray correct = {correct_bits, no_of_bits};
+
+	or_n(input0, input1, &output);
+
+	for (int i = 0; i < no_of_bits; i++) {
+		TEST_ASSERT_EQUAL_INT(correct.bits[i], output_bits[i]);
+	}
+}
+
+
 int main(void) {
 	UNITY_BEGIN();
 	
@@ -100,6 +144,8 @@ int main(void) {
 	RUN_TEST(test_nand_n_1);
 	RUN_TEST(test_and_n_0);
 	RUN_TEST(test_and_n_1);
+	RUN_TEST(test_or_n_0);
+	RUN_TEST(test_or_n_1);
 
 	return UNITY_END();
 }
