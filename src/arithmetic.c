5418f2517dfd5bedc0619bb551d7332616f7e2dd
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-10 04:38:12 -0400

    Made a smarter adder

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -7,35 +7,46 @@
 
 uint16_t half_adder_sum(uint16_t a, uint16_t b)
 {
+	a = a & 0b1;
+	b = b & 0b1;
 	return xor(a, b);
 }
 
 uint16_t half_adder_carry(uint16_t a, uint16_t b)
 {
+	a = a & 0b1;
+	b = b & 0b1;
 	return and(a, b);
 }
 
 uint16_t full_adder_sum(uint16_t a, uint16_t b, uint16_t carry_in)
 {
+	a = a & 0b1;
+	b = b & 0b1;
+	carry_in = carry_in & 0b1;
 	return xor(xor(a, b), carry_in);
 }
 
 uint16_t full_adder_carry(uint16_t a, uint16_t b, uint16_t carry_in)
 {
+	a = a & 0b1;
+	b = b & 0b1;
+	carry_in = carry_in & 0b1;
 	return or(or(and(a, b), and(a, carry_in)), and(b, carry_in));
 }
 
 uint16_t adder(uint16_t a, uint16_t b)
 {
+	uint16_t sum_bit = 0b0;
+	uint16_t carry_bit = 0b0;
+
 	uint16_t out = 0b0;
 
-	uint16_t mask;
-	uint16_t carry_in = 0b0;
-
 	for (int i = 0; i < 16; ++i) {
-		mask = 1 << i;
-		out |= full_adder_sum(and(a, mask), and(b, mask), carry_in);
-		carry_in = full_adder_carry(and(a, mask), and(b, mask), carry_in);
+		sum_bit = full_adder_sum((a >> i) & 1, (b >> i) & 1, carry_bit);
+		carry_bit = full_adder_carry((a >> i) & 1, (b >> i) & 1, carry_bit);
+
+		out |= sum_bit << i;
 	}
 
 	return out;
