9fd4f019b3dcdcee88769a5386c5c2dc88d73289
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-10 02:48:16 -0400

    Half Adder correct

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,8 +1,9 @@
 #include <stdint.h>
 
 #include "arithmetic.h"
+#include "gates.h"
 
 uint16_t half_adder_sum(uint16_t a, uint16_t b)
 {
-	return a;
+	return xor(a & 0b1, b & 0b1);
 }
--- tests/test_arithmetic.c
+++ tests/test_arithmetic.c
@@ -7,9 +7,23 @@ void tearDown(void) {}
 
 void test_half_adder_sum(void)
 {
-	uint16_t a = 0b0;
-	uint16_t b = 0b0;
+	uint16_t a;
+	uint16_t b;
 
+	a = 0b0;
+	b = 0b0;
+	TEST_ASSERT_EQUAL_INT(0, half_adder_sum(a, b));
+
+	a = 0b0;
+	b = 0b1;
+	TEST_ASSERT_EQUAL_INT(1, half_adder_sum(a, b));
+
+	a = 0b1;
+	b = 0b0;
+	TEST_ASSERT_EQUAL_INT(1, half_adder_sum(a, b));
+
+	a = 0b1;
+	b = 0b1;
 	TEST_ASSERT_EQUAL_INT(0, half_adder_sum(a, b));
 }
 
