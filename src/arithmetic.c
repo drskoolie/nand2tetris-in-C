e181f2423c9993ba6624f2fab8f7583768b7caf5
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-11 20:41:56 -0400

    Add ng calculation

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -73,4 +73,5 @@ void alu(uint16_t x, uint16_t y, uint16_t instruction_bits, uint16_t *out, uint1
 	*out = adder(x, y, ng);
 
 	*zr = comparater(*out, 0);
+	*ng = comparater(repeat_lsb((*out & 0b1000000000000000) >> 15), 0b1);
 }
--- tests/test_arithmetic.c
+++ tests/test_arithmetic.c
@@ -181,18 +181,20 @@ void test_alu_zx(void)
 
 	uint16_t out = 0b1111;
 	uint16_t zr = 1;
-	uint16_t ng = 0;
+	uint16_t ng = 1;
 
 	instruction_bits = 0b100000;
 	alu(x, y, instruction_bits, &out, &zr, &ng);
 	TEST_ASSERT_EQUAL_INT(0, out);
 	TEST_ASSERT_EQUAL_INT(1, zr);
+	TEST_ASSERT_EQUAL_INT(0, ng);
 
 	y = 10;
 	instruction_bits = 0b100000;
 	alu(x, y, instruction_bits, &out, &zr, &ng);
 	TEST_ASSERT_EQUAL_INT(y, out);
 	TEST_ASSERT_EQUAL_INT(0, zr);
+	TEST_ASSERT_EQUAL_INT(0, ng);
 }
 
 int main(void)
