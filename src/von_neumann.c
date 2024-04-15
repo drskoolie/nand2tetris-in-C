fe35d8c179443c677fae1942e1a9525ff6950f5c
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-15 17:13:27 -0400

    Add more instructions

--- src/von_neumann.c
+++ src/von_neumann.c
@@ -5,10 +5,16 @@
 void cpu(int16_t inM, int16_t instruction_bits, int16_t reset, flip_flop *reg_a)
 {
 
-	int16_t type_of_instruction;
-	int16_t value;
+	// Instruction Capturing
+	uint16_t type_of_instruction;
+	uint16_t comp_instruction;
+	uint16_t dest_instruction;
+	uint16_t jump_instruction;
 
 	type_of_instruction = instruction_bits >> 15;
+	comp_instruction = instruction_bits & 0b0001111111000000;
+	dest_instruction = instruction_bits & 0b0000000000111000;
+	jump_instruction = instruction_bits & 0b0000000000000111;
 
 	//Address Instruction (sets A register)
 	if (type_of_instruction == 0) {
