fe4d2eeca7597965c1cabe01b2fbcd14bb8c8030
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-15 17:06:55 -0400

    Add capture of type_of_instruction

--- include/von_neumann.h
+++ include/von_neumann.h
@@ -1,4 +1,8 @@
 #ifndef VON_NEUMANN_H
 #define VON_NEUMANN_H
 
+#include <stdint.h>
+
+void cpu(int16_t inM, int16_t instruction_bits, int16_t reset);
+
 #endif
--- src/von_neumann.c
+++ src/von_neumann.c
@@ -1,1 +1,18 @@
 #include "von_neumann.h"
+#include "arithmetic.h"
+
+void cpu(int16_t inM, int16_t instruction_bits, int16_t reset)
+{
+
+	int16_t type_of_instruction;
+
+	type_of_instruction = instruction_bits >> 15;
+
+	//Address Instruction (sets A register)
+	if (type_of_instruction == 0) {
+	}
+
+	//Compute Instruction
+	else if (type_of_instruction == 1) {
+	}
+}
