62bb438fe8cd449ae887cf7f58153034c62f43a2
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-22 19:57:04 -0400

    Address registering

--- tests/test_architecture.c
+++ tests/test_architecture.c
@@ -5,16 +5,27 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_cpu(void)
+void test_address_instruction(void)
 {
-	TEST_ASSERT_EQUAL_INT(0, 0);
+	memory_t ram;
+	registers_t regs;
+
+	initialize_memory(&ram, 100);
+	initialize_registers(&regs);
+
+	TEST_ASSERT_EQUAL_INT(0, get_register_A(&regs));
+	cpu(0b1, 0, &ram, &regs);
+	TEST_ASSERT_EQUAL_INT(1, get_register_A(&regs));
+
+	destroy_memory(&ram);
+	destroy_registers(&regs);
 }
 
 int main(void)
 {
 	UNITY_BEGIN();
 
-	RUN_TEST(test_cpu);
+	RUN_TEST(test_address_instruction);
 
 	return UNITY_END();
 }
