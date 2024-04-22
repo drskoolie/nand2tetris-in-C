e2dc9de47718d4732dda2d309c1e5f1011558f3c
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-22 04:30:39 -0400

    Remove legacy code

--- src/main.c
+++ src/main.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "sequential.h"
 
 int main()
 {
-	flip_flop *ff0, *ff1;
-
-	ff0 = initialize_flip_flop();
-	ff1 = initialize_flip_flop();
-	chain_flip_flops(ff0, ff1);
-	for (int i = 0; i < 5; i++) {
-
-		*(ff0->in) = 100;
-		printf("FF0\n");
-		print_flip_flop(ff0);
-		printf("FF1\n");
-		print_flip_flop(ff1);
-		tick_tock();
-
-		update_flip_flop(ff0);
-		update_flip_flop(ff1);
-	}
-
-	destroy_flip_flop(ff0);
-	destroy_flip_flop(ff1);
+	printf("Hello\n");
 }
