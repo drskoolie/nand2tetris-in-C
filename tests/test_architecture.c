eec154d32965a924a41d0a6d42a6fb7dffae9c65
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-22 20:51:22 -0400

    dest010 test added

--- tests/test_architecture.c
+++ tests/test_architecture.c
@@ -41,12 +41,32 @@ void test_cpu_mnem0_dest001(void)
 	destroy_registers(&regs);
 }
 
+void test_cpu_mnem0_dest010(void)
+{
+	memory_t ram;
+	registers_t regs;
+	int16_t instruction_bits;
+
+	instruction_bits = set_instruction_bits(0b1, 0b0, 0b111111, 0b010, 0b0);
+
+	initialize_memory(&ram, 128);
+	initialize_registers(&regs);
+
+	cpu(instruction_bits, 0, &ram, &regs);
+
+	TEST_ASSERT_EQUAL_INT(1, get_register_D(&regs));
+
+	destroy_memory(&ram);
+	destroy_registers(&regs);
+}
+
 int main(void)
 {
 	UNITY_BEGIN();
 
 	RUN_TEST(test_cpu_address_instruction);
 	RUN_TEST(test_cpu_mnem0_dest001);
+	RUN_TEST(test_cpu_mnem0_dest010);
 
 	return UNITY_END();
 }
